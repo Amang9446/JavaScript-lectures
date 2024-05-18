75c64c6c06028a6e8da7220eb2b2c1176d1249a5
amang9446 <dev.aman7571@gmail.com>
2024-05-18 15:56:05 +0530

    room Sockets

--- lecture13/index.js
+++ lecture13/index.js
@@ -1,21 +1,70 @@
+// const express = require("express")
+// const { WebSocketServer } = require("ws")
+// const app = express()
+// const httpServer = app.listen(3000, () => {
+//     console.log("Server is connected");
+// })
+// const wss = new WebSocketServer({
+//     server: httpServer
+// })
+// const clients = [];
+// wss.on('connection', function (ws) {
+//     ws.on('error', (error) => { console.log(error) })
+//     const userId = clients.length;
+//     clients.push({ ws, userId })
+//     ws.on('message', function (message) {
+//         clients.forEach((client) => {
+//             if (client.userId != userId) client.ws.send(`Client${userId} says ${message}`)
+//         })
+//     })
+//     ws.send("You are connected to Socket Server....")
+// })
+
+
+
 const express = require("express")
 const { WebSocketServer } = require("ws")
 const app = express()
 const httpServer = app.listen(3000, () => {
     console.log("Server is connected");
 })
+
 const wss = new WebSocketServer({
     server: httpServer
 })
+
 const clients = [];
+const room = {};
+
+// Set a password for the room
+const roomPassword = "12345678";
+
 wss.on('connection', function (ws) {
     ws.on('error', (error) => { console.log(error) })
-    const userId = clients.length;
-    clients.push({ ws, userId })
-    ws.on('message', function (message) {
-        clients.forEach((client) => {
-            if (client.userId != userId) client.ws.send(`Client${userId} says ${message}`)
-        })
+
+    // Prompt the client for the password
+    ws.send("Please enter the room password:");
+
+    ws.once('message', function (message) {
+        const enteredPassword = message.toString();
+
+        if (enteredPassword === roomPassword) {
+            const userId = clients.length;
+            clients.push({ ws, userId })
+            room[userId] = ws;
+
+            ws.on('message', function (message) {
+                for (const clientId in room) {
+                    if (clientId != userId) {
+                        room[clientId].send(`Client${userId} says ${message}`);
+                    }
+                }
+            })
+
+            ws.send("You Entered the room ");
+        } else {
+            ws.send("Incorrect password. Connection closed.");
+            ws.close();
+        }
     })
-    ws.send("You are connected to Socket Server....")
 })
